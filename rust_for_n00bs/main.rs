60dc5195ca4ebaa2d4a93715b2b1474da98a7164
Jullyano Lino <jullyanolino@gmail.com>
2024-09-23 10:42:46 -0300

    update main.rs with more lessons about Data Types (tuples)

--- rust_for_n00bs/main.rs
+++ rust_for_n00bs/main.rs
@@ -2,5 +2,11 @@ fn main(){
     let int: i8 = -20;
     let uint: u8 = 20;
 
-    println!("Hi");
+    let array[i32;5] = [1,2,3,4,5];
+
+    let tuple:(&str, &str, i32) = ("Charles", "Dickens", 1812);
+    println!("{} {} was bor in {}", tuple.0, tuple.1, tuple.2);
+
+    let (first_name:&str, last_name:&str, dob:i32) = tuple;
+    println!("{} {} was bor in {}", first_name, last_name, dob);
 }
